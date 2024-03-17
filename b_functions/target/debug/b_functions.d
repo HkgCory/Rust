33dde4326f19310719417300abe28a53b9338ec2
HkgCory <hkgkenneth@gmail.com>
2024-03-16 22:18:13 -0400

    test

---
+++ b_functions/target/debug/b_functions.d
@@ -0,0 +1,1 @@
+C:\Users\hkgke\Rust\b_functions\target\debug\b_functions.exe: C:\Users\hkgke\Rust\b_functions\src\main.rs
---
+++ b_functions/target/debug/deps/b_functions.d
@@ -0,0 +1,5 @@
+C:\Users\hkgke\Rust\b_functions\target\debug\deps\b_functions.exe: src\main.rs
+
+C:\Users\hkgke\Rust\b_functions\target\debug\deps\b_functions.d: src\main.rs
+
+src\main.rs:
